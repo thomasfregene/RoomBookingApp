a471a340cc0e72c259e47f0fdd4d0eb9478f1ae5
thomasfregene <fregzythomas@gmail.com>
2025-04-15 15:18:58 +0100

    Add project files.

---
+++ RoomBookingApp.Core/Processors/RoomBookingRequestProcessor.cs
@@ -0,0 +1,21 @@
+﻿using RoomBookingApp.Core.Models;
+
+namespace RoomBookingApp.Core.Processors
+{
+    public class RoomBookingRequestProcessor
+    {
+        public RoomBookingRequestProcessor()
+        {
+        }
+
+        public RoomBookingResult BookRoom(RoomBookingRequest bookinRequest)
+        {
+            return new RoomBookingResult
+            {
+                FullName = bookinRequest.FullName,
+                Date = bookinRequest.Date,
+                Email = bookinRequest.Email
+            };
+        }
+    }
+}
